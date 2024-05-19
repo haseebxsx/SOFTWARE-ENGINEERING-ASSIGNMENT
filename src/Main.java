6791dcba8fe7b4ef61b7b8d13260e8db08fd868b
Irfanxsx <seassignment2@haseebtalpur.com>
2024-05-19 13:07:32 +0100

    first commit (irfan)

--- src/Main.java
+++ src/Main.java
@@ -65,3 +65,165 @@ class Functions{
             return Expression;
         }
 }
+
+
+// Main code-------------------------------------------------------------------------------
+public class Echlon_form{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        // Taking Input
+        System.out.print("Enter an Expression_1 : ");
+        String Equation1 = sc.nextLine();
+
+        System.out.print("Enter an Expression_2 : ");
+        String Equation2 = sc.nextLine();
+
+        System.out.print("Enter an Expression_3 : ");
+        String Equation3 = sc.nextLine();
+
+        // Calling function in to an new Array
+        int[] Expression_1 = Functions.Split_Expression(Equation1);
+        int[] Expression_2 = Functions.Split_Expression(Equation2);
+        int[] Expression_3 = Functions.Split_Expression(Equation3);
+
+        Functions.Print_Expression(Expression_1);
+        Functions.Print_Expression(Expression_2);
+        Functions.Print_Expression(Expression_3);
+        System.out.println("_____________________ \n");
+
+        // Checking already have 1 at 0 index
+            if(Expression_2[0] == 1 && Expression_1[0] != 1){
+                int[] New_Expression = new int[3];
+                New_Expression =  Expression_1;
+                Expression_1 =  Expression_2;
+                Expression_2 =  New_Expression;
+
+                System.out.println("Row 1 <--> Row 2");
+
+                Functions.Print_Expression(Expression_1);
+                Functions.Print_Expression(Expression_2);
+                Functions.Print_Expression(Expression_3);
+                System.out.println("_____________________ \n");
+
+            }
+            if(Expression_3[0] == 1 && Expression_1[0] != 1){
+                int[] New_Expression = new int[3];
+                New_Expression =  Expression_1;
+                Expression_1 =  Expression_3;
+                Expression_3 =  New_Expression;
+
+                System.out.println("Row 1 <--> Row 3");
+
+                Functions.Print_Expression(Expression_1);
+                Functions.Print_Expression(Expression_2);
+                Functions.Print_Expression(Expression_3);
+                System.out.println("_____________________ \n");
+            }        
+        
+        
+        if(Expression_1[0] != 1){
+
+        // Divide    
+        Functions.Divide(0, Expression_1);
+        
+        Functions.Print_Expression(Expression_1);
+        Functions.Print_Expression(Expression_2);
+        Functions.Print_Expression(Expression_3);
+        System.out.println("_____________________ \n");
+        }
+
+        //Subtract row 2
+        if(Expression_2[0] != 0){
+        Functions.Subtract(0, Expression_2, Expression_1);
+
+        Functions.Print_Expression(Expression_1);
+        Functions.Print_Expression(Expression_2);
+        Functions.Print_Expression(Expression_3);
+        System.out.println("_____________________ \n");
+        }
+
+        // Subtract row 3
+        if(Expression_3[0] != 0){
+        Functions.Subtract(0, Expression_3, Expression_1);
+
+        Functions.Print_Expression(Expression_1);
+        Functions.Print_Expression(Expression_2);
+        Functions.Print_Expression(Expression_3);
+        System.out.println("_____________________ \n");
+        }
+
+        //Divide row 2
+        if(Expression_2[1] != 1){
+        Functions.Divide(1, Expression_2);
+
+        Functions.Print_Expression(Expression_1);
+        Functions.Print_Expression(Expression_2);
+        Functions.Print_Expression(Expression_3);
+        System.out.println("_____________________ \n");
+        }
+
+        //Subtract row 3
+        if(Expression_3[1] != 0){
+        Functions.Subtract(1, Expression_3, Expression_2);
+
+        Functions.Print_Expression(Expression_1);
+        Functions.Print_Expression(Expression_2);
+        Functions.Print_Expression(Expression_3);
+        System.out.println("_____________________ \n");
+        }
+
+        // Divide row 3 ==> 1
+        if(Expression_3[2] != 1 && Expression_3[2] != 0){
+            Functions.Divide(2, Expression_3);
+
+            Functions.Print_Expression(Expression_1);
+            Functions.Print_Expression(Expression_2);
+            Functions.Print_Expression(Expression_3);
+            System.out.println("_____________________ \n");            
+            
+        }
+
+        // Subtract row 2 ==> 0
+        if(Expression_2[2] != 0){
+        Functions.Subtract(2, Expression_2, Expression_3);
+
+        Functions.Print_Expression(Expression_1);
+        Functions.Print_Expression(Expression_2);
+        Functions.Print_Expression(Expression_3);
+        System.out.println("_____________________ \n"); 
+
+        }
+
+        //Subtract row 1 ==> 0
+        if(Expression_1[2] != 0){
+            Functions.Subtract(2, Expression_1, Expression_3);
+    
+            Functions.Print_Expression(Expression_1);
+            Functions.Print_Expression(Expression_2);
+            Functions.Print_Expression(Expression_3);
+            System.out.println("_____________________ \n"); 
+    
+        }
+
+        // Subtract Row 1 ==> 0
+        if(Expression_1[1] != 0){
+            Functions.Subtract(1, Expression_1, Expression_2);
+    
+            Functions.Print_Expression(Expression_1);
+            Functions.Print_Expression(Expression_2);
+            Functions.Print_Expression(Expression_3);
+            System.out.println("_____________________ \n"); 
+
+                    // Showing Result 
+        System.out.println("x  = " + Expression_1[3] );
+        System.out.println("y  = " + Expression_2[3] );
+        System.out.println("z  = " + Expression_3[3] );
+
+
+        sc.close();
+    
+        }
+        
+    }
+}
